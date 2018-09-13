05bbdfb77a715bc08e817993c685c63fd2d03a11
Kyle Chu <ktnc.985@gmail.com>
2018-09-14 00:13:30 +0800

    move global variables to a separate config header file

---
+++ Arduino/arduino_rtos/config.h
@@ -0,0 +1,21 @@
+// Configuration file containing all the global constants
+
+// Currently assume that configTICK_RATE_HZ is 1000 hz, delay is in ticks
+
+// System variables
+#define STACK_SIZE       200
+#define SENSOR_COUNT     1
+#define USB_DEBUG_MODE   1
+
+//buffer sizes
+#define RAW_BUFF_SIZE    100
+#define MSG_LEN          (2 * SENSOR_COUNT) + 1 
+
+// Task delays
+#define READ_DATA_DELAY  500
+#define MSG_FORMAT_DELAY 600
+#define SEND_DELAY       3000
+
+
+
+
