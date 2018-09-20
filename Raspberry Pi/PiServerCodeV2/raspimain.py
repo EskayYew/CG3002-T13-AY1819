9243bdabbd1df1e734d111472dbb16f650f49e8f
Kyle Chu <ktnc.985@gmail.com>
2018-09-20 13:59:00 +0800

    RaspiMain Integration v0.2

--- Raspberry Pi/PiServerCodeV2/raspimain.py
+++ Raspberry Pi/PiServerCodeV2/raspimain.py
@@ -16,7 +16,7 @@ class MachineLearning(threading.Thread):
         self.buffer = bufferX
    
     def processData(self, bufferY):
-        print(bufferY[0])
+        print(bufferY)
         return 'chicken'
 
     def processAction(self):
@@ -31,47 +31,88 @@ class MachineLearning(threading.Thread):
 
         self.buffer.reset()
         self.databuffer = []
-        threading.Timer(10, self.processAction).start()
+        threading.Timer(2, self.processAction).start()
 
     def run(self):
-        threading.Timer(60, self.processAction).start()
+        threading.Timer(5, self.processAction).start()
 
 class Receiver(threading.Thread):
-    def __init__(self, SENSOR_COUNT):
+    def __init__(self, SENSOR_COUNT, bufferX):
+        threading.Thread.__init__(self)
         self.SENSOR_COUNT = SENSOR_COUNT
         self.msg_len = (2 * self.SENSOR_COUNT) + 1 
         
+        self.buffer = bufferX
         # Setup serial port
         self.ser = serial.Serial('/dev/ttyS0', 115200)
+        self.ser.reset_input_buffer()
+        self.ser.reset_output_buffer()
+        
         self.data_buff = []
         self.chksum = b'0'
         self.is_id = True
+        self.messages_recieved = 0
 
     def handshake(self):
         # Wait for SYNC packet
-        while(self.ser.read() != b'0'):
+        while(self.ser.read() != b'\x00'):
             pass
-        print("connection with arduino established")
+        #print("connection with arduino established")
         self.ser.write(b'1')
         # Wait for SYNC-ACK packet
-        while(self.ser.read() != b'2'):
+        while(self.ser.read() != b'\x02'):
             pass
 
-        print("handshake with arduino established")
-
-    def printSelf(self):
-        # newTime = time.time() + 5
-
+        #print("handshake with arduino established")
+    
+        
+    def communicate(self):
+    #while True:
+        while (len(self.data_buff) < self.msg_len):
+            data = self.ser.read()
+            #print(data)
+            if (data != b'\r' and data != b'\n'):
+                #print(ord(data))
+                if not self.is_id:
+                    self.chksum = bytes(x ^ y for x, y in zip(self.chksum, data))
+                self.data_buff.append(data)
+                self.is_id = not self.is_id
+        # print(chksum)
+        # print(data_buff)
+        if (str(ord((self.data_buff[self.msg_len - 1]))) == self.chksum.decode()):
+            self.messages_recieved += 1
+            #print("Data received and verified")
+            print('{} messages received'.format(self.messages_recieved))
+            self.ser.write(b'1')
+        else:
+            self.ser.write(b'6')
+            print("Checksum error")
+            print('expected: {} recv: {}'.format(str(ord((self.data_buff[self.msg_len - 1]))), self.chksum.decode()))
+        # Wait for FIN packet
+        while(self.ser.read() != b'\x07'):
+            pass
+        self.ser.write(b'7')
+        while(self.ser.read() != b'\x01'):
+            pass
+        #print("Message received")
+        #print(self.data_buff)
         processLock.acquire()
-        print('I am the Receiver')
+        self.buffer.append(self.data_buff)
         processLock.release()
+        self.data_buff = []
+        self.is_id = True
+        self.chksum = b'0'
 
+    def receiveLoop(self):
+        # newTime = time.time() + 5
+        self.handshake()
+        self.communicate()
         # threading.Timer(newTime - time.time(), self.printSelf).start()
-        threading.Timer(0.05, self.printSelf).start()
+        threading.Timer(0.05, self.receiveLoop).start()
 
     def run(self):
-        self.printSelf()
-        
+        self.receiveLoop()
+
 class Pi:
     def __init__(self):
         self.dataList = [0, 0, 0, 0]
@@ -81,9 +122,8 @@ class Pi:
     def main(self):
         SENSOR_COUNT = 5
 
-        receiver = Receiver(SENSOR_COUNT) 
-        receiver.handshake()
-
+        receiver = Receiver(SENSOR_COUNT, self.buffer)
+        
         machine = MachineLearning(self.buffer)
 
         self.threads.append(machine)
