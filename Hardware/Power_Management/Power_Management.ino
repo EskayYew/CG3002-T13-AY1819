ca72efeeaed7fe02bb10cfbc5ee546f29fe2f24d
Eskay-PC\Eskay <yew_sheng_kai@hotmail.com>
2018-10-10 02:26:31 +0800

    Power_Management update.

--- Hardware/Power_Management/Power_Management.ino
+++ Hardware/Power_Management/Power_Management.ino
@@ -1,23 +1,38 @@
+#include "Time.h"
 #define NUM_SAMPLES 20
 
 // Constants
-const int SENSOR_PIN_A0 = A0;   // Input pin for measuring Vout from INA169
-const int SENSOR_PIN_A1 = A1;   // Input pin for measuring Vout from voltage divider
-const float RS = 0.1;           // Shunt resistor value (in ohms) for INA169
-const float VOLTAGE_REF = 5.0;  // Reference voltage for analog read, //5.126 <-- voltage regulator
-const float R1 = 22000.0;       // Resistor 22k ohms (10% tolerance)
-const float R2 = 22000.0;       // Resistor 22k ohms with 20% tolerance from R1 26888.889/25545.455/20431 with 5v
+const int SENSOR_PIN_A0 = A0;         // Input pin for measuring Vout from INA169
+const int SENSOR_PIN_A1 = A1;         // Input pin for measuring Vout from voltage divider
+const float RS = 0.1;                 // Shunt resistor value (in ohms) for INA169
+const float VOLTAGE_REF = 4.875;        // Reference voltage for analog read, //5.126 <-- voltage regulator
+const float R1 = 22000.0;             // Resistor 22k ohms (10% tolerance)
+const float R2 = 22000.0;             // Resistor 22k ohms with 20% tolerance from R1 26888.889/25545.455/20431 with 5v
 
 // Global Variables
-unsigned char sample_count = 0; // current sample number
-float sensorValueA0;            // Variable to store value from analog read
-float sensorValueA1;            // Variable to store value from analog read/sum of samples taken
-float voltage;                  // Calculated Voltage Out value, Vout = Vin * R2/(R1+R2)
-float voltageIn;                // Calculated Voltage Source value
-float current;                  // Calculated current value
-float power;                    // Calculated power value
-float lastPower = 0.0;          // Previous power value
-float energy;                   // Calculated energy value
+unsigned char sample_count = 0;       // current sample number
+float sensorValueA0 = 0.0;            // Variable to store value from analog read
+float sensorValueA1 = 0.0;            // Variable to store value from analog read/sum of samples taken
+float voltageOut = 0.0;               // Calculated Voltage Out value, Vout = Vin * R2/(R1+R2)
+float voltageIn = 0.0;                // Calculated Voltage In Source value
+float current = 0.0;                  // Calculated current value
+float totalCurrent = 0.0;             // Calculated Total current value
+float avgAmp = 0.0;                   // Calculated avgAmp
+float ampHr = 0.0;                    // Calculated amp-hour
+float power = 0.0;                    // Calculated power value
+float lastPower = 0.0;                // Previous power value
+float energy = 0.0;                   // Calculated energy value
+float elapsedTime = 0.0;
+
+float timer; // convert milliseconds to seconds
+
+void printDigits(int digits) {
+  // utility function for digital clock display: prints preceding colon and leading 0
+  Serial.print(":");
+  if (digits < 10)
+  Serial.print('0');
+  Serial.print(digits);
+}
 
 void setup() {
   // Initialize serial monitor
@@ -25,7 +40,8 @@ void setup() {
 }
 
 void loop() {
-
+  elapsedTime = millis() / 1000.0;
+  
   // Read analog value
   // take a number of analog samples and add them up
   while (sample_count < NUM_SAMPLES) {
@@ -35,47 +51,60 @@ void loop() {
       delay(10);
   }
 
-  // Remap the ADC value into a voltage number (5.23V reference)
-  sensorValueA0 = (sensorValueA0 / (float)NUM_SAMPLES * VOLTAGE_REF) / 1024;
-  voltage = (sensorValueA1 / (float)NUM_SAMPLES * VOLTAGE_REF) / 1024.0;
+  // Actual voltage calibration: 
+  // 2.73v 5.47v analogRead: 560
+  // 1 is equivalent to 2.73/560=4.875mv
+  // Vout = (4.875*sample)/1000
+  // Actual battery voltage = (2*Vout)
+  // Remap the ADC value into a voltage number
+  sensorValueA0 = (sensorValueA0 / (float)NUM_SAMPLES * VOLTAGE_REF) / 1024.0;
+  voltageOut = (sensorValueA1 / (float)NUM_SAMPLES * VOLTAGE_REF) / 1024.0;
 
-  // Calculate VoltageIn from Vout and two resistor
+  // Voltage value
+  // Calculate Vin from Vout and two resistor
   // Battery V = 5.224, Voltage across resistor = 2.608
   // division factor = 2.001533742331288343558282208589
-  voltageIn = voltage * 2;
-//  voltageIn = (voltage * (R1+R2)) / R2;
-//  voltageIn = voltage * 2.001533742331288343558282208589;
-  
-  // Follow the equation given by the INA169 datasheet to
+  voltageIn = voltageOut * 2;
+
+  // Current value
+  // Following the equation given by the INA169 datasheet to
   // determine the current flowing through RS. Assume RL = 10k
   // Is = (Vout x 1k) / (RS x RL)
-  current = (sensorValueA0 / (RS * 10.0)) * 1000;
-
-  // Power value
-  power = current * voltage;
-
-  // Energy value
-  energy = ((power + lastPower) / 2) * 500 / 3600000;
-  lastPower = lastPower + power;
+  current = (sensorValueA0 / (RS * 10.0));
+  totalCurrent = (totalCurrent + current * 1000) ;
+  avgAmp = totalCurrent / elapsedTime;
+  ampHr = (avgAmp * elapsedTime) / 3600; //mAh
   
-  // Output value (in amps/watts/voltage/joules) to the serial monitor to 2 decimal
+  // Power value
+  power = current * voltageIn;
+  
+  // Energy value
+  energy = (power * elapsedTime) / 3600;
+
+  // Output value (in amps/amps-hour/watts/watts-hour/voltage/time) to the serial monitor to 2 decimal
   // places
   Serial.print(current, 2);
-  Serial.print(" mA | ");
+  Serial.print(" A | ");
+  Serial.print(ampHr, 2);
+  Serial.print(" mAh | ");
   Serial.print(power, 2);
-  Serial.print(" mW | ");
-  Serial.print(voltage, 2);
-  Serial.print(" V | ");
+  Serial.print(" W | ");
+  Serial.print(energy, 2);
+  Serial.print(" Wh | ");
+  Serial.print(voltageOut, 2);
+  Serial.print(" Vout | ");
   Serial.print(voltageIn,2 );
   Serial.print(" Vin | ");
-  Serial.print(energy, 2);
-  Serial.println(" J");
+  Serial.print("Uptime: ");
+  Serial.print(hour());
+  printDigits(minute());
+  printDigits(second());
+  Serial.println();
 
   sample_count = 0;
   sensorValueA0 = 0;
   sensorValueA1 = 0;
   // Delay program for a few milliseconds
-  delay(500);
-
+  delay(800);
 }
 
