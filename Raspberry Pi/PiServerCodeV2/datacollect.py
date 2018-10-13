11a59abde87fa20bcaf2aa8e1b26efdc6b5138bf
Darren <zen_darren@hotmail.com>
2018-10-13 12:16:39 +0800

    Updated datacollect.py

--- Raspberry Pi/PiServerCodeV2/datacollect.py
+++ Raspberry Pi/PiServerCodeV2/datacollect.py
@@ -42,22 +42,59 @@ class MachineLearning(threading.Thread):
         threading.Timer(5, self.processAction).start()
 
 class Receiver(threading.Thread):
-    def __init__(self, SENSOR_COUNT, bufferX):
+    def __init__(self, bufferX):
         threading.Thread.__init__(self)
-        self.SENSOR_COUNT = SENSOR_COUNT
-        self.msg_len = SENSOR_COUNT
+        
+        self.SENSOR_COUNT = 26
+        self.msg_len = self.SENSOR_COUNT * 2
         
         self.buffer = bufferX
         # Setup serial port
-        self.ser = serial.Serial('/dev/ttyS0', 115200)
+        self.ser = serial.Serial(            
+            port='/dev/ttyS0',
+            baudrate = 115200,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            bytesize=serial.EIGHTBITS,
+            timeout=0.1
+        )
+
         self.ser.reset_input_buffer()
         self.ser.reset_output_buffer()
         
         self.data_buff = []
-        self.chksum = 0
-        self.is_id = True
+        self.byteArray = []
+        
+    def checkByteArray(self, bArray):
+        newArray = []
+        checksum = 0
 
-    def handshake(self):
+        chkPos = self.SENSOR_COUNT-1
+
+        # go from 0 to 24 since sensorcount is 25(50,51)
+        for counter in range (0, chkPos):
+            byte1 = bArray[counter * 2]
+            checksum = checksum ^ (int.from_bytes(byte1, byteorder="big", signed=True))
+            byte2 = bArray[counter * 2 + 1]
+            checksum = checksum ^ (int.from_bytes(byte2, byteorder="big", signed=True))
+            combinedValue = int.from_bytes(byte1 + byte2, byteorder="big", signed=True)
+            
+            newArray.append(combinedValue)
+
+        chkPos = chkPos*2
+
+        # checksum is at 50 to 51 position of bArray
+        # if checksum matches, then data is clean and ready to be stored into circular buffer
+        if (checksum == (int.from_bytes(bArray[chkPos] + bArray[chkPos+1] , byteorder="big", signed=True))):            
+            self.ser.write(b'1')
+            return newArray
+        else:
+            self.ser.write(b'6')
+            print("Checksum error")
+            return None
+
+    def communicate(self):
+
         # Wait for SYNC packet
         while(self.ser.read() != b'\x00'):
             pass
@@ -67,41 +104,22 @@ class Receiver(threading.Thread):
         while(self.ser.read() != b'\x02'):
             pass
 
-        #print("handshake with arduino established")
-    
-        
-    def communicate(self):
-    #while True:
-    # read data sequentially and store into self.data_buff
-    # do checksum and compare with checksum that is on the last position of data_buff 
+        # handshake established with handshake flag
+
+        #will be receiving 52 array bytes, 0 to 51
         while (len(self.data_buff) < self.msg_len):
-            lowByte = self.ser.read()
-            highByte = self.ser.read()
-            data = (highByte << 8) | lowByte
+            rcv = self.ser.read()
+            if (rcv != b'\r' and rcv != b'\n'):
+                self.byteArray.append(rcv)
 
-            # data = self.ser.read()
-            # print(data)
-            self.chksum ^= data
-            self.data_buff.append(data)
-
-        # print(chksum)
-
-        print(self.data_buff)
+        self.data_buff = self.checkByteArray(self.byteArray)
         
-        # if checksum matches, then data is clean and ready to be stored into circular buffer
-        if (self.data_buff[self.msg_len - 1] == self.chksum):
-            
-            # print("Data received and verified")
-            # must lock when feeding data to buffer. 
+        if(self.data_buff is not None):
             processLock.acquire()
             self.buffer.append(self.data_buff)
-            print(self.buffer.getSize)
             processLock.release()
-            
-            self.ser.write(b'1')
-        else:
-            self.ser.write(b'6')
-            print("Checksum error")
+
+        print(self.data_buff)
 
         # Wait for FIN packet
         while(self.ser.read() != b'\x07'):
@@ -110,15 +128,15 @@ class Receiver(threading.Thread):
         while(self.ser.read() != b'\x01'):
             pass
 
-        self.data_buff = []
+        self.byteArray.clear()
+        self.data_buff.clear()
         self.chksum = 0
 
     def receiveLoop(self):
         # newTime = time.time() + 5
-        self.handshake()
         self.communicate()
         # threading.Timer(newTime - time.time(), self.printSelf).start()
-        threading.Timer(0.05, self.receiveLoop).start()
+        threading.Timer(0.1, self.receiveLoop).start()
 
     def run(self):
         self.receiveLoop()
@@ -131,9 +149,7 @@ class Pi:
 
     def main(self):
         try:
-            SENSOR_COUNT = 26
-
-            receiver = Receiver(SENSOR_COUNT, self.buffer)
+            receiver = Receiver(self.buffer)
             
             machine = MachineLearning(self.buffer)
 
