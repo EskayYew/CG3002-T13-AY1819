32bb12db477b5d6556fb69c4a63efba9979a53f6
Kyle Chu <ktnc.985@gmail.com>
2018-10-18 14:23:28 +0800

    switch out of computer testing mode

--- Arduino/arduino_rtos/arduino_rtos.ino
+++ Arduino/arduino_rtos/arduino_rtos.ino
@@ -60,44 +60,44 @@ int16_t sensorValueA1 = 0;            // Variable to store value from analog rea
 float voltageOut = 0.0;               // Calculated Voltage Out value, Vout = Vin * R2/(R1+R2)
 float voltageIn = 0.0;                // Calculated Voltage In Source value
 float current = 0.0;                  // Calculated current value
-float totalCurrent = 0.0;             // Calculated Total current value
-float avgAmp = 0.0;                   // Calculated avgAmp
-float ampHr = 0.0;                    // Calculated amp-hour
+// float totalCurrent = 0.0;             // Calculated Total current value
+// float avgAmp = 0.0;                   // Calculated avgAmp
+// float ampHr = 0.0;                    // Calculated amp-hour
 float power = 0.0;                    // Calculated power value
-float lastPower = 0.0;                // Previous power value
+// float lastPower = 0.0;                // Previous power value
 float energy = 0.0;                   // Calculated energy value
-float elapsedTime = 0.0;
+// float elapsedTime = 0.0;
 
 void setup() {
   //Initialize mpu
   pinMode(FLEXPIN, INPUT);
-//  Wire.begin();
-//  tcaselect(0);
-//  Wire.beginTransmission(MPU);
-//  Wire.write(0x6B); 
-//  Wire.write(0);    
-//  Wire.endTransmission(true);
-//  tcaselect(1);
-//  Wire.beginTransmission(MPU);
-//  Wire.write(0x6B); 
-//  Wire.write(0);    
-//  Wire.endTransmission(true);
-//  tcaselect(2);
-//  Wire.beginTransmission(MPU);
-//  Wire.write(0x6B); 
-//  Wire.write(0);    
-//  Wire.endTransmission(true);
-//  tcaselect(3);
-//  Wire.beginTransmission(MPU);
-//  Wire.write(0x6B); 
-//  Wire.write(0);    
-//  Wire.endTransmission(true);
+  Wire.begin();
+  tcaselect(0);
+  Wire.beginTransmission(MPU);
+  Wire.write(0x6B); 
+  Wire.write(0);    
+  Wire.endTransmission(true);
+  tcaselect(1);
+  Wire.beginTransmission(MPU);
+  Wire.write(0x6B); 
+  Wire.write(0);    
+  Wire.endTransmission(true);
+  tcaselect(2);
+  Wire.beginTransmission(MPU);
+  Wire.write(0x6B); 
+  Wire.write(0);    
+  Wire.endTransmission(true);
+  tcaselect(3);
+  Wire.beginTransmission(MPU);
+  Wire.write(0x6B); 
+  Wire.write(0);    
+  Wire.endTransmission(true);
   // Initialize serial communication - 115200 bps,8 Data, 1 Stop, 0 Parity
   // Temporarily using Serial for debugging, will move over to Serial
   if (USB_DEBUG_MODE) {
     Serial.begin(115200, SERIAL_8N1);
   }
-  Serial.begin(115200, SERIAL_8N1);
+  Serial1.begin(115200, SERIAL_8N1);
 
   /*
    * Initialize semaphores
@@ -140,8 +140,8 @@ void setup() {
  * e.g data = 0xABCD - CD is transmitted first, followed by AB
  */
  void send_16bit(uint16_t data) {
-  Serial.write(data);
-  Serial.write(data >> 8);
+  Serial1.write(data);
+  Serial1.write(data >> 8);
  }
 /*
  * Select which channelt to read the data from
@@ -160,7 +160,7 @@ void tcaselect(uint8_t i) {
 void establish_connection() {
   uint8_t received_byte = NONE;
   uint16_t resend_timer = 0;
-  Serial.write(SYNC);
+  Serial1.write(SYNC);
   if (USB_DEBUG_MODE) {
     Serial.println(SYNC);
   }
@@ -169,19 +169,19 @@ void establish_connection() {
     // TODO - add time out
     while(!Serial.available()) {
       if (resend_timer == 1000) {
-        Serial.write(SYNC); 
+        Serial1.write(SYNC); 
         resend_timer = 0;       
       } else {
         resend_timer++;
       }
     }
     // -48 since serial monitor/pi sends ascii characters
-    received_byte = Serial.read() - 48;
+    received_byte = Serial1.read() - 48;
     if (USB_DEBUG_MODE) {
       Serial.println(received_byte);
     }
   }
-  Serial.write(SYNC_ACK);
+  Serial1.write(SYNC_ACK);
   if (USB_DEBUG_MODE) {
     Serial.println(SYNC_ACK);
   } 
@@ -198,17 +198,11 @@ void transmit_data_from_buffer() {
     uint8_t *current_msg_ptr =  msg_buffer.data_buffer[msg_buffer.strt];
     while (received_byte != ACK) {
       //Transmit data
-//       for(int i = 0; i < MSG_LEN; i++ ) {
-//         send_16bit(current_msg_ptr[i]);
-//          if (USB_DEBUG_MODE){
-//            Serial.println(current_msg_ptr[i]);
-//          } 
-//       }
-      Serial.write(current_msg_ptr, MSG_LEN);
+      Serial1.write(current_msg_ptr, MSG_LEN);
       
       // Wait for reponse from RPi
-      while(!Serial.available()) {};
-      received_byte = Serial.read() - 48;
+      while(!Serial1.available()) {};
+      received_byte = Serial1.read() - 48;
     }
     //update buffer state and allow current data to be overwritten
     msg_buffer.buff_size_remaining++;
@@ -225,17 +219,17 @@ void transmit_data_from_buffer() {
 */
 void terminate_connection(){
   uint8_t received_byte = NONE;
-  Serial.write(FIN);
+  Serial1.write(FIN);
   if (USB_DEBUG_MODE) {
     Serial.println(FIN);
   }
   while (received_byte != FIN) {
     // Wait until response received from RPi
     // TODO - add time out
-    while(!Serial.available()) {};
-    received_byte = Serial.read() - 48;
+    while(!Serial1.available()) {};
+    received_byte = Serial1.read() - 48;
   }
-  Serial.write(ACK);
+  Serial1.write(ACK);
   if (USB_DEBUG_MODE) {
     Serial.println(ACK);
   }
@@ -270,9 +264,9 @@ void update_power_readings() {
       // determine the current flowing through RS. Assume RL = 10k
       // Is = (Vout x 1k) / (RS x RL)
       current = (sensorValueA0 / (RS * 10.0));
-      totalCurrent = (totalCurrent + current * 1000) ;
-      avgAmp = totalCurrent / elapsedTime;
-      ampHr = (avgAmp * elapsedTime) / 3600; //mAh
+      //totalCurrent = (totalCurrent + current * 1000) ;
+      //avgAmp = totalCurrent / elapsedTime;
+      //ampHr = (avgAmp * elapsedTime) / 3600; //mAh
       
       // Power value
       power = current * voltageIn;
@@ -325,11 +319,11 @@ void TaskReadData(void *pvParameters) {
         uint8_t checksum = 0;
         int16_t msg_buffer_index = 0;
         for (int i = 0; i < MPU_COUNT; i++) {
-          // tcaselect(i);
-          // Wire.beginTransmission(MPU);
-          // Wire.write(0x3B);  
-          // Wire.endTransmission(false);
-          // Wire.requestFrom(MPU,12,true);  
+          tcaselect(i);
+          Wire.beginTransmission(MPU);
+          Wire.write(0x3B);  
+          Wire.endTransmission(false);
+          Wire.requestFrom(MPU,12,true);  
 
           /* SENSOR IDs
            * j = 0 - 2 : AcX,Y,Z
@@ -337,8 +331,8 @@ void TaskReadData(void *pvParameters) {
            * j = 4 - 6 : GyX,Y,Z
            */
             for (int j = 0; j < SENSOR_PER_MPU; j++) {
-              int16_t data = j + 1;
-              //int16_t data = Wire.read()<<8|Wire.read();
+              // int16_t data = j + 1;
+              int16_t data = Wire.read()<<8|Wire.read();
               if (j != 3) {
                 uint8_t lsb = (data & 0x00FF);
                 uint8_t msb = (data & 0xFF00) >> 8;
@@ -350,16 +344,14 @@ void TaskReadData(void *pvParameters) {
               }
             }
           }
-        int16_t data = 27;
-        uint8_t lsb = (data & 0x00FF);
-        uint8_t msb = (data & 0xFF00) >> 8;
+        // int16_t Flex = 27;
+        int16_t Flex = digitalRead(FLEXPIN);
+        uint8_t lsb = (Flex & 0x00FF);
+        uint8_t msb = (Flex & 0xFF00) >> 8;
         checksum ^= lsb;
         current_msg_ptr[msg_buffer_index++] = lsb;
         checksum ^= msb;
         current_msg_ptr[msg_buffer_index++] = msb;
-        //int16_t Flex = digitalRead(FLEXPIN);
-        //checksum ^= 0;
-        //current_msg_ptr[msg_buffer_index++] = Flex;
 
         //Get power readings - METHOD 1
         for(int k = 0; k < 4; k++) {
--- Arduino/arduino_rtos/config.h
+++ Arduino/arduino_rtos/config.h
@@ -6,12 +6,13 @@
 #define STACK_SIZE       200
 #define SENSOR_PER_MPU   7
 #define MPU_COUNT        4
+#define POWER_DATA_COUNT 4
 #define USB_DEBUG_MODE   0
 
 //buffer sizes
 #define RAW_BUFF_SIZE    100
 // + 1 for flex sensor and + 1 for checksum
-#define MSG_LEN          2 * ((MPU_COUNT * (SENSOR_PER_MPU - 1)) + 1) + 1  
+#define MSG_LEN          2 * (POWER_DATA_COUNT + (MPU_COUNT * (SENSOR_PER_MPU - 1)) + 1) + 1  
 
 // Task delays
 #define READ_DATA_DELAY  200
