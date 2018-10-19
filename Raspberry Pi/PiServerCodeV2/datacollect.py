eacb6738947168708bd4fcb72e7b76208705efc4
Darren <zen_darren@hotmail.com>
2018-10-19 20:57:12 +0800

    Updated DataCollect.py without error handling

--- Raspberry Pi/PiServerCodeV2/datacollect.py
+++ Raspberry Pi/PiServerCodeV2/datacollect.py
@@ -70,12 +70,13 @@ class Receiver(threading.Thread):
         # checksum is at 50th position of bArray
         # if checksum matches, then data is clean and ready to be stored into circular buffer
         if (checksum == arrayChecksum):            
-            self.ser.write(b'1')
+            self.ser.write(b'C')
             return newArray
         else:
-            self.ser.write(b'6')
+            self.ser.write(b'N')
             print("Checksum error")
-            
+            print(bArray)
+            print(newArray)
             print(checksum)
             print(arrayChecksum)
             
@@ -83,21 +84,37 @@ class Receiver(threading.Thread):
     
     def establish_connection(self):
         # Wait for SYNC packet
-        while(self.ser.read() != b'\x00'):
+        while(self.ser.read() != b'S'):
             pass
-        self.ser.write(b'1')
+        self.ser.write(b'A')
         # Wait for SYNC-ACK packet
-        while(self.ser.read() != b'\x02'):
+        while(self.ser.read() != b'H'):
             pass
+        #self.ser.reset_input_buffer()
+        #self.ser.reset_output_buffer()
         self.connection_established = True
+        print("Connection Established")
     
     def read_data(self):
         #will be receiving 52 array bytes, 0 to 51
-        timeout_counter = 0
         while (len(self.byteArray) < 47):
+            startTime = time.time()
+            while(self.ser.in_waiting == 0):
+                currentTime = time.time()
+                if (currentTime - startTime >= 2.0):
+                    self.byteArray = []
+                    self.data_buff = []
+                    self.connection_established = False
+                    #self.ser.reset_input_buffer()
+                    #self.ser.reset_output_buffer()
+                    print('termination')
+                    return
+                
             rcv = self.ser.read()
-            if (rcv != b'\r' and rcv != b'\n'):
-                self.byteArray.append(rcv)
+            self.byteArray.append(rcv)
+        #    if (rcv != b'\r' and rcv != b'\n'):
+        #        self.byteArray.append(rcv)
+                
                 
         self.data_buff = self.checkByteArray(self.byteArray)
         
@@ -109,13 +126,14 @@ class Receiver(threading.Thread):
         if(self.buffer.getSize() % 25 == 0):
             print(self.buffer.getSize())
 
+        #self.ser.reset_input_buffer()
         self.byteArray = []
         self.data_buff = []
            
     def communicate(self):
         if not self.connection_established:
             self.establish_connection()
-            self.read_data()
+            #self.read_data()
         else:
             self.read_data()
         #print("Starting data tranfser...")
@@ -135,7 +153,7 @@ class Pi:
     def __init__(self):
         self.dataList = [0, 0, 0, 0]
         self.threads = []
-        self.buffer = CircleBuffer(1500)
+        self.buffer = CircleBuffer(750)
 
     def main(self):
         try:
