737df802b435a2585835211999eb33eea0307dcc
Darren <zen_darren@hotmail.com>
2018-10-26 18:52:15 +0800

    Implemented new datacollecting script
    No need for anymore keyboard interrupt. Auto exits once buffer is maxed.
    Included some minor checks for RaspiMain.py

---
+++ Raspberry Pi/PiV3/RaspiMain.py
@@ -0,0 +1,222 @@
+import threading
+import time
+import serial
+import socket
+import sys
+import base64
+from NeuralNet_Model import DanceClassifierNN
+from Crypto import Random
+from Crypto.Cipher import AES
+from RingBuffer import RingBuffer
+
+class Communication:
+
+    def __init__(self, host, port, dataList):
+        self.host = host
+        self.port = int(port)
+        self.bs = 16
+        self.key = bytes("1234123412341234".encode("utf8"))
+        self.dataList = dataList
+
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect((self.host, self.port))
+        print("[+] You are currently connected to ", self.host + ":" + str(self.port))
+
+        self.actions = {'WIPERS':'wipers', 'NUMBER':'number7', 'CHICKE':'chicken',
+            'SIDEST':'sidestep', 'TURNCL':'turnclap', 'LOGOUT':'logout'}
+
+    def encrypt(self, message):
+        text = self._pad(message)
+        iv = Random.new().read(AES.block_size)
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return base64.b64encode(iv + cipher.encrypt(text))
+
+    def updateData(self, arrayData):
+        self.dataList = arrayData
+
+    def _pad(self, s):
+        return bytes(s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs), 'utf-8')
+
+    def format(self, message):
+        return '#{0}|{1:.2f}|{2:.2f}|{3:.2f}|{4:.2f}|'.format(self.actions[message], self.dataList[0], self.dataList[1], self.dataList[2], self.dataList[3])
+
+    def sendMessage(self, message):
+        message = self.format(message)
+        message = self.encrypt(message)
+        self.client.send(message)    
+
+class Pi:
+    def __init__(self, host, port):
+        self.WINDOWSIZE = 150
+        self.dataList = [4.65, 2.00, 1.98, 10.00]
+        self.buffer = RingBuffer(self.WINDOWSIZE)
+        self.client = Communication(host, port, self.dataList)
+        
+        # Machine Learning
+        self.model = DanceClassifierNN()
+        self.executionTime = 0.0
+
+        # Data Collection
+        self.SENSOR_COUNT = 23
+        self.energy = 0.000
+        self.connection_established = False
+
+        # Setup serial port
+        self.ser = serial.Serial(            
+            #port = 'COM5',
+            port='/dev/ttyS0',
+            baudrate = 115200,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            bytesize=serial.EIGHTBITS,
+            #timeout=0.1
+        )
+        self.ser.flushInput()
+        self.ser.flushOutput()
+
+        self.ser.reset_input_buffer()
+        self.ser.reset_output_buffer()
+        
+        self.data_buff = []
+        self.byteArray = []
+
+    def checkByteArray(self, bArray):
+        newArray = []
+        checksum = 0
+
+        chkPos = self.SENSOR_COUNT
+
+        # go from 0 to 22 since sensorcount is 23(46)
+        for counter in range (0, chkPos):
+            byte1 = bArray[counter * 2]
+            checksum = checksum ^ (int.from_bytes(byte1, byteorder="big", signed=True))
+            byte2 = bArray[counter * 2 + 1]
+            checksum = checksum ^ (int.from_bytes(byte2, byteorder="big", signed=True))
+            combinedValue = int.from_bytes(byte2 + byte1, byteorder="big", signed=True)
+            
+            if counter >= 19 and counter <= 21:
+                combinedValue /= 100
+            if counter == 22:
+                combinedValue /= 1000
+            
+            newArray.append(combinedValue)
+
+        self.dataList = newArray[19:23]
+
+        self.client.updateData(self.dataList)
+        
+        chkPos = chkPos*2
+
+        arrayChecksum = (int.from_bytes(bArray[chkPos] , byteorder="big", signed=True))
+        # checksum is at 46th position of bArray
+        # if checksum matches, then data is clean and ready to be stored into circular buffer
+        if (checksum == arrayChecksum):
+            self.ser.write(b'C')
+            if checksum == 0:
+                return None
+            
+            return newArray[0:19]
+        else:
+            self.ser.write(b'N')
+            
+            print("Checksum error")
+            print(bArray)
+            print(newArray)
+            print(checksum)
+            print(arrayChecksum)
+            
+            return None
+    
+    def establish_connection(self):
+        # Wait for SYNC packet
+        while(self.ser.read() != b'S'):
+            pass
+        self.ser.write(b'A')
+        # Wait for SYNC-ACK packet
+        while(self.ser.read() != b'H'):
+            pass
+
+        self.connection_established = True
+        print("Connection Established")
+    
+    def read_data(self):
+        # will be receiving 47 array bytes, 0 to 46
+        while (len(self.byteArray) < 47):
+            startTime = time.time()
+            while(self.ser.in_waiting == 0):
+                currentTime = time.time()
+                if (currentTime - startTime >= 2.0):
+                    self.byteArray = []
+                    self.data_buff = []
+                    self.connection_established = False
+                    print('termination')
+                    return
+                
+            rcv = self.ser.read()
+            self.byteArray.append(rcv)
+                            
+        self.data_buff = self.checkByteArray(self.byteArray)
+        
+        if(self.data_buff is not None):
+            self.buffer.append(self.data_buff)
+            
+        if(self.buffer.getSize() % 30 == 0):
+            print(self.buffer.getSize())
+
+        self.byteArray = []
+        self.data_buff = []
+           
+    def communicate(self):
+        if not self.connection_established:
+            self.establish_connection()
+            #self.read_data()
+        else:
+            self.read_data()
+
+    def processData(self, feedingBuffer):
+        newPrediction = self.model.detectMove(feedingBuffer)
+
+        if(newPrediction is None):
+            print('ML returning None')
+            return 'IDLE_A'
+
+        return newPrediction[0]
+
+    def main(self):
+        try:
+            self.executionTime = time.time()
+            while True:
+                # Keep collecting data
+                self.communicate()
+                
+                if(self.buffer.getSize() == self.WINDOWSIZE):
+                    action = 'IDLE_A'
+                    
+                    tempBuffer = self.buffer.get()
+                    feedingBuffer = []
+
+                    for i in range(0, self.WINDOWSIZE):
+                        feedingBuffer += tempBuffer[i]
+                    
+                    action = self.processData(feedingBuffer)
+                    currentTime = time.time()
+                    
+                    if((currentTime - self.executionTime) >= 4.0):
+                        print(action)
+                        if action != 'IDLE_A':
+                            print(action)
+                            self.client.sendMessage(action)
+                        self.executionTime = currentTime                   
+
+                    self.buffer.reset()
+                    # print('Buffer is cleared.')
+                    
+        except KeyboardInterrupt:
+            print('Program End')
+            sys.exit(1)           
+
+if __name__ == '__main__':
+    host = sys.argv[1]
+    port = sys.argv[2]
+    pi = Pi(host, port)
+    pi.main()
---
+++ Raspberry Pi/PiV3/datacollect.py
@@ -0,0 +1,145 @@
+import time
+import serial
+import csv
+import sys
+from CircleBuffer import CircleBuffer
+
+class Pi:
+    def __init__(self):
+        self.WINDOWSIZE = 2000
+        self.buffer = CircleBuffer(self.WINDOWSIZE)
+        self.SENSOR_COUNT = 23
+        self.connection_established = False
+        # Setup serial port
+        self.ser = serial.Serial(            
+            #port = 'COM5',
+            port='/dev/ttyS0',
+            baudrate = 115200,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            bytesize=serial.EIGHTBITS,
+            #timeout=0.1
+        )
+        self.ser.flushInput()
+        self.ser.flushOutput()
+
+        self.ser.reset_input_buffer()
+        self.ser.reset_output_buffer()
+        
+        self.data_buff = []
+        self.byteArray = []
+        
+    def checkByteArray(self, bArray):
+        newArray = []
+        checksum = 0
+
+        chkPos = self.SENSOR_COUNT
+
+        # go from 0 to 22 since sensorcount is 23(46)
+        for counter in range (0, chkPos):
+            byte1 = bArray[counter * 2]
+            checksum = checksum ^ (int.from_bytes(byte1, byteorder="big", signed=True))
+            byte2 = bArray[counter * 2 + 1]
+            checksum = checksum ^ (int.from_bytes(byte2, byteorder="big", signed=True))
+            combinedValue = int.from_bytes(byte2 + byte1, byteorder="big", signed=True)
+            #checksum = combinedValue ^ checksum
+            
+            if counter >= 19 and counter <= 21:
+                combinedValue /= 100
+            if counter == 22:
+                combinedValue /= 1000
+            
+            newArray.append(combinedValue)
+        
+        chkPos = chkPos*2
+
+        arrayChecksum = (int.from_bytes(bArray[chkPos] , byteorder="big", signed=True))
+        # checksum is at 46th position of bArray
+        # if checksum matches, then data is ready to be stored into circular buffer
+        if (checksum == arrayChecksum):            
+            self.ser.write(b'C')
+
+            if checksum == 0:
+                return None
+
+            return newArray
+        else:
+            self.ser.write(b'N')
+            print("Checksum error")
+            print(bArray)
+            print(newArray)
+            print(checksum)
+            print(arrayChecksum)
+            
+            return None
+    
+    def establish_connection(self):
+        # Wait for SYNC packet
+        while(self.ser.read() != b'S'):
+            pass
+        self.ser.write(b'A')
+        # Wait for SYNC-ACK packet
+        while(self.ser.read() != b'H'):
+            pass
+
+        self.connection_established = True
+        print("Connection Established")
+    
+    def read_data(self):
+        #will be receiving 47 array bytes, 0 to 46
+        while (len(self.byteArray) < 47):
+            startTime = time.time()
+            while(self.ser.in_waiting == 0):
+                currentTime = time.time()
+                if (currentTime - startTime >= 2.0):
+                    self.byteArray = []
+                    self.data_buff = []
+                    self.connection_established = False
+                    print('termination')
+                    return
+                
+            rcv = self.ser.read()
+            self.byteArray.append(rcv)                
+                
+        self.data_buff = self.checkByteArray(self.byteArray)
+        
+        if(self.data_buff is not None):
+            self.buffer.append(self.data_buff)
+            
+        if(self.buffer.getSize() % 50 == 0):
+            print(self.buffer.getSize())
+
+        self.byteArray = []
+        self.data_buff = []
+           
+    def communicate(self):
+        if not self.connection_established:
+            self.establish_connection()
+        else:
+            self.read_data()
+        
+    def recordData(self):
+        file = str(sys.argv[1])
+        databuffer = self.buffer.get()
+        myFile = open(file, 'w', newline='')
+        with myFile:
+            writer = csv.writer(myFile)
+            for items in databuffer:
+                writer.writerow(items)
+        myFile.close()
+        print("Writing complete")
+        print('Program End')
+        sys.exit(1)
+
+    def main(self):
+        try:
+            while True:
+                self.communicate()
+                if(self.buffer.getSize == self.WINDOWSIZE):
+                    self.recordData()
+        except KeyboardInterrupt:
+            self.recordData()
+
+if __name__ == '__main__':
+    pi = Pi()
+    pi.main()
