5cb7407c64344042ad2b7a1b012c6d19e9b2f338
Teo Meng Shin, Ryan <wanganpro@gmail.com>
2018-10-26 21:26:29 +0800

    Update SVM training code.

--- Dance classifier (Ryan)/SVM/trainSVM.py
+++ Dance classifier (Ryan)/SVM/trainSVM.py
@@ -1,34 +1,36 @@
 import ExtractFeatures
 import ReadCSVToList
+import SegmentData
+
 import numpy as np
 
 SAVE_FLAG = False #Change to true to save the model.
 SAVED_MODEL_NAME = "SVM"
 SAVED_SCALER_NAME = "SVM_Scaler"
 
-CHICKEN_FILE = "Training/CHICKENSegmented.csv"
+CHICKEN_FOLDER = "Training/Chicken/"
 CHICKEN_LABEL = "CHICKE"
-CHICKEN_DATA = (CHICKEN_FILE, CHICKEN_LABEL)
+CHICKEN_DATA = (CHICKEN_FOLDER, CHICKEN_LABEL)
 
-IDLE_FILE = "Training/IDLE_ACTIONSegmented.csv"
+IDLE_FOLDER = "Training/Idle/"
 IDLE_LABEL = "IDLE_A"
-IDLE_DATA = (IDLE_FILE, IDLE_LABEL)
+IDLE_DATA = (IDLE_FOLDER, IDLE_LABEL)
 
-NUMBER7_FILE = "Training/NUMBER7Segmented.csv"
+NUMBER7_FOLDER = "Training/Number 7/"
 NUMBER7_LABEL = "NUMBER"
-NUMBER7_DATA = (NUMBER7_FILE, NUMBER7_LABEL)
+NUMBER7_DATA = (NUMBER7_FOLDER, NUMBER7_LABEL)
 
-SIDESTEP_FILE = "Training/SIDESTEPSegmented.csv"
+SIDESTEP_FOLDER = "Training/Sidestep/"
 SIDESTEP_LABEL = "SIDEST"
-SIDESTEP_DATA = (SIDESTEP_FILE, SIDESTEP_LABEL)
+SIDESTEP_DATA = (SIDESTEP_FOLDER, SIDESTEP_LABEL)
 
-TURNCLAP_FILE = "Training/TURNCLAPSegmented.csv"
+TURNCLAP_FOLDER = "Training/Turnclap/"
 TURNCLAP_LABEL = "TURNCL"
-TURNCLAP_DATA = (TURNCLAP_FILE, TURNCLAP_LABEL)
+TURNCLAP_DATA = (TURNCLAP_FOLDER, TURNCLAP_LABEL)
 
-WIPERS_FILE = "Training/WIPERSSegmented.csv"
+WIPERS_FOLDER = "Training/Wipers/"
 WIPERS_LABEL = "WIPERS"
-WIPERS_DATA = (WIPERS_FILE, WIPERS_LABEL)
+WIPERS_DATA = (WIPERS_FOLDER, WIPERS_LABEL)
 
 DATA_FILES = [CHICKEN_DATA, IDLE_DATA, NUMBER7_DATA, SIDESTEP_DATA, TURNCLAP_DATA, WIPERS_DATA]
 
@@ -37,16 +39,16 @@ FINAL_TRAINING_DATA = []
 
 def loadData():
     print("Labels used are:")
-    for training_file in DATA_FILES:
-        filename = training_file[0]
-        label = training_file[1]
+    for training_data in DATA_FILES:
+        folder = training_data[0]
+        label = training_data[1]
         counter = 0
-        temp_data = ReadCSVToList.convertFileToList(filename)
+        temp_data = SegmentData.processFiles(folder)
         print(label)
         
         for row in temp_data:
-            ExtractFeatures.extractFeatures(row)
-            FINAL_TRAINING_DATA.append(row)
+            processedRow = ExtractFeatures.extractFeatures(row)
+            FINAL_TRAINING_DATA.append(processedRow)
             TRAINING_LABELS.append(label)
     
     print("Done loading training data!\n")
