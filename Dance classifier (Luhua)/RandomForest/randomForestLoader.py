fe26d672a1dfe10cb6a709f80942bb4b0fded0d3
YangLuhua <e0012658@u.nus.edu>
2018-11-02 15:37:36 +0800

    New random forest model object

--- Dance classifier (Luhua)/RandomForest/randomForestLoader.py
+++ Dance classifier (Luhua)/RandomForest/randomForestLoader.py
@@ -3,45 +3,49 @@ import numpy as np
 from scipy.stats import kurtosis
 
 
-rdf_module = joblib.load("randomforest")
+class RandomForestModel:
+    def __init__(self):
+        self.model = joblib.load("randomforest")
 
+    def predict(self, data):
+        if len(data) != 150:
+            print("ERROR: Window size does not match!")
+            print("Expected window size is " + str(150) + ". Input size: " + str(len(data)))
+            return None
 
-def random_forest_loader(data):
-    data_1 = extract_features(data[0:48])
-    data_2 = extract_features(data[48:96])
-    data_3 = extract_features(data[96:144])
-    result_1 = rdf_module.predict(data_1)
-    result_2 = rdf_module.predict(data_2)
-    result_3 = rdf_module.predict(data_3)
+        data_1 = self.extract_features(data[0:48])
+        data_2 = self.extract_features(data[48:96])
+        data_3 = self.extract_features(data[96:144])
+        result_1 = self.model.predict(data_1)
+        result_2 = self.model.predict(data_2)
+        result_3 = self.model.predict(data_3)
 
-    if result_1 == result_2:
-        return result_1
+        if result_1 == result_2:
+            return result_1
 
-    elif result_1 == result_3:
-        return result_2
+        elif result_1 == result_3:
+            return result_2
 
-    elif result_2 == result_3:
-        return result_3
+        elif result_2 == result_3:
+            return result_3
 
-    else:
-        return ['Unsure']
+        else:
+            return ['Unsure']
 
+    def extract_features(self, window):
+        result_row = []
 
-def extract_features(window):
+        for i in range(0, window[0].size-5):
 
-    result_row = []
+            average = np.average(window[0:, i])
+            variance = np.var(window[0:, i])
+            _max = np.max(window[0:, i])
+            _min = np.min(window[0:, i])
+            kts = kurtosis(window[0:, i])
+            result_row.append(average)
+            result_row.append(variance)
+            result_row.append(_max)
+            result_row.append(_min)
+            result_row.append(kts)
 
-    for i in range(0, window[0].size-5):
-
-        average = np.average(window[0:, i])
-        variance = np.var(window[0:, i])
-        max = np.max(window[0:, i])
-        min = np.min(window[0:, i])
-        kts = kurtosis(window[0:, i])
-        result_row.append(average)
-        result_row.append(variance)
-        result_row.append(max)
-        result_row.append(min)
-        result_row.append(kts)
-
-    return result_row
+        return result_row
